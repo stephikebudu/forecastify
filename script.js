11d528151d90d6eefd35a4c7c767d01fa9ce88a0
Stephany Ikebudu <stephikebudu@gmail.com>
2024-03-27 01:50:35 +0100

    implement function to display weather in app

--- script.js
+++ script.js
@@ -16,16 +16,58 @@ function getWeather() {
                             })
                             .catch((error) => {
                                 console.error("Error getting weather data", error);
-                                throw new Error("Error getting weather data, try later.");
+                                alert("Error getting weather data, try again later.");
                             }); 
                             
     fetch(forecastUrl).then((response) => response.json())
                         .then((data) => {
-                            displayForecast(data.list);
+                            displayHourlyForecast(data.list);
                         })
                         .catch((err) => {
                             console.error("Error getting forecast data", err);
-                            throw new Error("Error getting forecast data, try later.")
+                            alert("Error getting forecast data, try again later.")
                         })
 }
 
+function displayWeather(data) {
+    const tempDiv = document.getElementById("temp-div");
+    const weatherInfoDiv = document.getElementById("weather-info");
+    const weatherIcon = document.getElementById("weather-icon");
+    const hourlyForecastDiv = document.getElementById("hourly-forecast");
+
+    // Reset content
+    tempDiv.innerHTML = "";
+    weatherInfoDiv.innerHTML = "";
+    hourlyForecastDiv.innerHTML = "";
+
+    // check if data was retrieved. Get useful data from json if retrieved and handle error if not
+    if (data.code = "404") {
+        weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML = `<p>${temperature}</p>`;
+        const weatherHTML = `
+            <p>${cityName}</p>
+            <p>${description}</p>
+        `;
+
+        // put newly created elements in container divs
+        tempDiv.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = description;
+
+        showIcon(); //call function to show Icon
+    }
+}
+
+// function to show icon 
+function showIcon() {
+    const weatherIcon = document.getElementById("weather-icon");
+    weatherIcon.style.display = "block";
+}
