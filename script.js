ccaa62b914a871ee9fec26d9fdaa3de7a1f9b60b
Stephany Ikebudu <stephikebudu@gmail.com>
2024-03-27 01:52:17 +0100

    display and style forecast data in app

--- script.js
+++ script.js
@@ -71,3 +71,25 @@ function showIcon() {
     const weatherIcon = document.getElementById("weather-icon");
     weatherIcon.style.display = "block";
 }
+
+function displayHourlyForecast(hourlyData) {
+    const hourlyForecastDiv = document.getElementById("hourly-forecast");
+    const next24Hours = hourlyData.slice(0, 8); //get next 24 hours (N/b: it is in 3-hour intervals)
+
+    next24Hours.forEach((item) => {
+        const dateTime = new Date(item.dt * 1000) //convert timestamp to milliseconds
+        const hours = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15); //Convert temp value to celsius
+        const iconCode = item.weather[0].icon;
+        const iconUrl = "https://openweathermap.org/img/wn/${iconCode}.png";
+        const hourlyItemHTML = `
+            <div>
+                <span>${hour}:00</span>
+                <img src="${iconUrl}" alt="Weather icon" />
+                <span>${temperature}</span>
+            </div>
+        `;
+
+        hourlyForecastDiv.innerHTML += hourlyItemHTML;
+    });
+}
