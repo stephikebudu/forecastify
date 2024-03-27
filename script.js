7b7d2859ceeb157a6837aae87f26279eedaa5387
Stephany Ikebudu <stephikebudu@gmail.com>
2024-03-27 01:16:05 +0100

    include function for fetching forecast data

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ function getWeather() {
         return;
     }
 
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    
-    fetch(weatherUrl).then((response) => response.json())
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(currentWeatherUrl).then((response) => response.json())
                             .then((data) => {
                                 displayWeather(data);
                             })
                             .catch((error) => {
                                 console.error("Error getting weather data", error);
                                 throw new Error("Error getting weather data, try later.");
-                            });                    
+                            }); 
+                            
+    fetch(forecastUrl).then((response) => response.json())
+                        .then((data) => {
+                            displayForecast(data.list);
+                        })
+                        .catch((err) => {
+                            console.error("Error getting forecast data", err);
+                            throw new Error("Error getting forecast data, try later.")
+                        })
 }
 
